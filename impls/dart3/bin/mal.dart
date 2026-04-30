bcfe80e468d1f1603cdf733eed1a2296fd0bdefb
Krysl <krysl@qq.com>
2026-04-30 12:59:12 +0800

    dart3: init a empty dart3 project
    Signed-off-by: Krysl <krysl@qq.com>

---
+++ impls/dart3/bin/mal.dart
@@ -0,0 +1,5 @@
+import 'package:mal/mal.dart' as mal;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mal.calculate()}!');
+}
---
+++ impls/dart3/test/mal_test.dart
@@ -0,0 +1,8 @@
+import 'package:mal/mal.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
